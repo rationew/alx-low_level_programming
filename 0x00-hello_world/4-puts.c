4806a2d1d74b25ab5e2faf8f7de44b79bfe9ef7a
rationew <julieynjoroge@gmail.com>
2022-09-12 11:26:16 -0700

    hello printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /** 
- *Description: main - print out a sring to the stdout
+ *Description:main-print out a sring to the stdout
+ *
  * Return: 0 if success
  */
 int main(void)
-
 {
 	puts("Programming is like building a multilingual puzzle\n");
-
 	return (0);
-
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Description: main - prints out the string to the stdout
+ *
+ * Return: 0 is success
+ */
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

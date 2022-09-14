4fed28007054ec248604f228444f156d4c4c7e04
rationew <julieynjoroge@gmail.com>
2022-09-14 14:56:55 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,10 +9,10 @@ void print_alphabet(void)
 {
 	char a = 'a';
 
-         while (a <= 'z')
-	 {
-		 _putchar(a);
-		 a++;
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
 	}
-	 _putchar('\n');
+	_putchar('\n');
 }

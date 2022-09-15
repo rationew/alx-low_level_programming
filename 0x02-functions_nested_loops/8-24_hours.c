ccf56f6f5f4200c935f1bae5934af872990f7351
rationew <julieynjoroge@gmail.com>
2022-09-15 09:03:45 -0700

    prints every minute of the day of Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * jack_bauer: prints every minute of the day of Jack Bauer
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Description: prints every minute of the day
+ */

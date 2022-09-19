58aa4c6db91241b8225e3da6f5947c347ba08ed3
rationew <julieynjoroge@gmail.com>
2022-09-19 09:54:27 -0700

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,13 +1,16 @@
+#include "main.h"
+#include "stdio.h"
+
 /**
- *  main - check the code
+ * main - check the code
  *
- *  Return: Always 0
+ * Return: Always 0
  */
-int main(void)
+int main()
 {
 	int n;
 
-	n = 402;
+	n=402;
 	printf("n=%d\n", n);
 	reset_to_98(&n);
 	printf("n=%d\n", n);

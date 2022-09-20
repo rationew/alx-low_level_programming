99896d386031b3554086a3288e75bfab4b3ec7d3
rationew <julieynjoroge@gmail.com>
2022-09-20 13:02:59 -0700

    task 7

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,18 +3,17 @@
 /**
  * puts2 - prints every character of a string
  * @str: string to print
- *
  * Return: void
  */
 void puts2(char *str)
 {
 	int i;
+
 	while (*(str + i))
 
 	{
 		 _putchar(*(str + i));
 		 i = i + 2;
 	}
-	
 	_putchar('\n');
 }

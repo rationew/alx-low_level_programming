64dfa946adb0dea0da10a6f807b40efaf9c523fd
rationew <julieynjoroge@gmail.com>
2022-10-04 13:42:47 -0700

    libmy

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * _isdigit - Write a function that checks for a digit 0-9.
+  * @c: parameter for function.
+  *
+  * Return: Always 0.
+  */
+int _isdigit(int c)
+{
+}
---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string up to n
+ * @dest: Destination of the pointer to the string
+ * @src: Source of the pointer to the string to copy
+ * @n: Number of bytes to copy.
+ *
+ * Return: Pointer to the destination string.
+ */
+char *_strncpy(char *dest, char *src, int n)
+
+{
+}

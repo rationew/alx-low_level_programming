01ae52e8b1125f7234d3189975e2e0f11c76ab7a
rationew <julieynjoroge@gmail.com>
2022-10-06 11:29:04 -0700

    string

---
+++ 0x0C-more_malloc_free/1-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *concat;
+
+	concat = string_nconcat("Best ", "School !!!", 6);
+	printf("%s\n", concat);
+	free(concat);
+	return (0);
+}

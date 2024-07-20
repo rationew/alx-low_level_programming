3dd643d7e41394e65522e227b30ed797f46888ce
Juliet Njoroge <julieynjoroge@gmail.com>
2024-07-20 03:42:16 -0700

    Clean up residual submodule information

--- 100-jump.c
+++ 100-jump.c
@@ -8,6 +8,7 @@
  * @array: Pointer to the first element of the array to search in.
  * @size: Number of elements in the array.
  * @value: Value to search for.
+ *
  * Return: The first index where the value is located, or -1 if not found.
  */
 int jump_search(int *array, size_t size, int value)
@@ -24,6 +25,8 @@ int jump_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
 		prev += step;
+		if (prev >= size)
+			break;
 	}
 
 	printf("Value found between indexes [%lu] and [%lu]\n", prev - step, prev);

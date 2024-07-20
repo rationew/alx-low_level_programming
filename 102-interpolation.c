23fe8237bec57f157e6e835bbef5384b144e89eb
Juliet Njoroge <julieynjoroge@gmail.com>
2024-07-20 07:43:01 -0700

    Interpolation search

---
+++ 102-interpolation.c
@@ -0,0 +1,55 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * interpolation_search - Searches for a value in a sorted array using
+ *                        interpolation search
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: The index of the value in the array, or -1 if not found or if
+ *         array is NULL
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t low, high, pos;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	low = 0;
+	high = size - 1;
+
+	while (low <= high && value >= array[low] && value <= array[high])
+	{
+		if (low == high)
+		{
+			if (array[low] == value)
+				return (low);
+			return (-1);
+		}
+
+		pos = low + (((double)(high - low) / (array[high] - array[low])) *
+			     (value - array[low]));
+
+		if (pos >= size)
+		{
+			printf("Value checked array[%zu] is out of range\n", pos);
+			return (-1);
+		}
+
+		printf("Value checked array[%zu] = [%d]\n", pos, array[pos]);
+
+		if (array[pos] == value)
+			return (pos);
+
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+	}
+
+	return (-1);
+}
+
--- search_algos.h
+++ search_algos.h
@@ -3,9 +3,19 @@
 
 #include <stddef.h>
 
+/**
+ * interpolation_search - Searches for a value in a sorted array using interpolation search
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ * 
+ * Return: The index of the value in the array, or -1 if not found or if array is NULL
+ */
+int interpolation_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
+
 
